607ae8e6bbfee79d140d02e05bb26aac1968d740
zaine-907 <139459416+zaine-907@users.noreply.github.com>
2024-08-03 17:56:02 +0500

    added index.html

---
+++ script.js
@@ -0,0 +1,23 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+
+Array.from(buttons).forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML === '=') {
+            try {
+                string = eval(string);
+                document.querySelector('input').value = string;
+            } catch (err) {
+                document.querySelector('input').value = "Error";
+            }
+        }
+        else if (e.target.innerHTML=='C') {
+            string ="";
+            document.querySelector('input').value = string;
+        }
+         else {
+            string += e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+    });
+});
